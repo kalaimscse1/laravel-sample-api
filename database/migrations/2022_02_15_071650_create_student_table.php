ff9d80c70cc7c5624a577a72b98ea25d6eabefa1
Kalaivanan-1996 <57209314+kalaimscse1@users.noreply.github.com>
2022-02-15 12:58:20 +0530

    Add files via upload

---
+++ database/migrations/2022_02_15_071650_create_student_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('regno');
+            $table->string('email',255)->unique();
+            $table->integer('mobile');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
